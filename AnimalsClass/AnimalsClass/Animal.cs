7f671ed79bd9d8bc02efb1bda296e6c4038ba627
snatallia <snatallia@gmail.com>
2019-03-21 16:44:29 +0300

    Animal commit

---
+++ AnimalsClass/AnimalsClass/Animal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AnimalClass
+{
+    /// <summary>
+    /// Animal class - essence aminal
+    /// animal can eat, walk, growl
+    /// 
+    /// </summary>
+    public class Animal
+    {
+        public string Kind { get; set; }
+
+        public Animal(string kind)
+        {
+            Kind = kind;
+        }
+
+        public string Eat()
+        {
+            return $"Animal {Kind} eats.";
+        }
+
+        public string Walk()
+        {
+            return $"Animal {Kind} walks.";
+        }
+
+        public string Growl()
+        {
+            return $"Animal {Kind} growls.";
+        }
+    }
+}
---
+++ AnimalsClass/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using AnimalClass;
+using System.Windows;
+
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Animal animal = new Animal("Lion");
+
+        public MainWindow()
+        {
+            InitializeComponent();            
+            lblAnimal.Content = animal.Kind;
+        }
+
+        private void btnEat_Clicked(object sender, RoutedEventArgs e)
+        {
+            lblAnimal.Content = $"Kind: {animal.Kind} \nAction: {animal.Eat()}";
+        }
+
+        private void btnWalk_Clicked(object sender, RoutedEventArgs e)
+        {
+            lblAnimal.Content = $"Kind: {animal.Kind} \nAction: {animal.Walk()}";
+        }
+
+        private void btnGrowl_Clicked(object sender, RoutedEventArgs e)
+        {
+            lblAnimal.Content = $"Kind: {animal.Kind} \nAction: {animal.Growl()}";
+        }
+    }
+}
